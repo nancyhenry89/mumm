9bc8bf5f6d9df883c5b4771356cb8b3770f35d9b
Nancy <nancyhenry89@gmail.com>
2017-12-20 15:25:48 +0200

    Masterpage

---
+++ new-design/footer.php
@@ -0,0 +1,49 @@
+<footer>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="logo">
+                    <img src="assets/img/logo.png" alt="logo"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <button class="btn btn-primary">Contact our Customer Productivity Team</button>
+            </div>
+            <div class="col-md-4">
+                <button class="btn btn-primary">Download A PDF fo this website</button>
+            </div>
+            <div class="col-md-4">
+                <button class="btn btn-primary">Download Sample Menu</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="social">
+                    <div class="social-item">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </div>
+                    <div class="social-item">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </div>
+                    <div class="social-item">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copy-rights">
+                    © 2017 Mumm · All Rights Reserved
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
---
+++ new-design/header.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+  <title>Mumm</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<header>
+  <div class="container">
+  <div class="row">
+    <div class="col-md-6 logo">
+      <img src="assets/img/logo.png" alt="logo"/>
+    </div>
+    <div class="col-md-2">
+      <button class="btn btn-primary">Schedule Order</button>
+    </div>
+    <div class="col-md-2">
+      <div class="account-info">
+        <div class="name">Mohamed Ahmed</div>
+        <div class="balance">
+          <label>Available Balance</label>
+          <span>EGP 873</span>
+        </div>
+      </div>
+      <div class="expand-account">
+          <i class="fa fa-caret-down" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="col-md-1">
+      <div class="cart">
+        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+      </div>
+    </div>
+    </div>
+  </div>
+</header>
+
