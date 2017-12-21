d0d551acde38a3fdbbdd0222ab5a6fce23eb03eb
Nancy <nancyhenry89@gmail.com>
2017-12-21 14:40:55 +0200

    Login + landing partial

---
+++ new-design/landing.php
@@ -0,0 +1,70 @@
+<?php require_once('header.php'); ?>
+    <div class="container-fluid landing">
+        <div class="container-fluid landingBG">
+            <div class="container banner">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h2>Home cooked goodness,</h2>
+                    <h2>delivered fresh to your office!</h2>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <button class="btn btn-primary">Get started</button>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <a class="link">Order for a meeting</a>
+                  </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="container about text-center">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>What is mumm?</h3>
+              <p>Mumm is an online platform connecting talented homebased cooks with hungry professionals to give them access to wholesome fresh homemade food</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <h3>What we do?</h3>
+              <p>Corporate offers to Elimnate Time wasted in your team's food ordering, No setup no cleanup to implement a corporate food program, Enhance company culture and collaboration, No Longterm Commitment- Unsubscribe Anytime, Satisfy "almost" everytaste with a daily changeble menu</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <h3>How it works</h3>
+              <div class="steps">
+                <div class="row">
+                  <div class="col-md-4">
+                      <img class="imgstep-img" src="assets/img/step1.png" alt="step 1"/>
+                      <div class="step-title">Each employee chooses his/her dishes</div>
+                  </div>
+                  <div class="col-md-4">
+                      <img class="imgstep-img" src="assets/img/step2.png" alt="step 2"/>
+                      <div class="step-title">Food arrives on time to your office</div>
+                  </div>
+                  <div class="col-md-4">
+                      <img class="imgstep-img" src="assets/img/step3.png" alt="step 3"/>
+                      <div class="step-title">Dishes are rated and choices are optimized to employee ratings</div>
+                  </div>
+                </div>
+                <div class="row steps-line">
+                  <div class="col-md-4"><div>1</div></div>
+                  <div class="col-md-4"><div>2</div></div>
+                  <div class="col-md-4"><div>3</div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container info text-center">
+            <h4 class="sp">No need to pay the full price of food!</h4>
+            <div>mumm can work a system where your company subsidise part of the food subscription and the employee pays part of the subscription.</div>
+            <div>Atleast <span class="sp">20% saving</span> for employees vs any other comparable meal on the market</div>
+        </div>
+  </div>
+<?php require_once('footer.php');?>
