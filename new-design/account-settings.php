a23dfeb1b77a4322c55280c113bef31024161b41
Nancy <nancyhenry89@gmail.com>
2017-12-21 12:29:40 +0200

    Account settings page

--- new-design/account-settings.php
+++ new-design/account-settings.php
@@ -1,3 +1,110 @@
 <?php require_once('header.php'); ?>
-
+    <div class="container account-settings">
+    <h2>Account Settings</h2>
+    <h3>Profile</h3>
+    <div class="row form profile">
+            <div class="col-md-6">
+                <label>First name</label>
+                <input name="first-name"/>
+            </div>
+            <div class="col-md-6">
+                <label>Last name</label>
+                <input name="last-name"/>
+            </div>
+            <div class="col-md-12">
+                <label>E-mail address</label>
+                <input name="email"/>
+            </div>
+            <div class="col-md-12">
+                <label>Phone no.</label>
+                <input name="phone"/>
+            </div>
+            <div class="col-md-12">
+                <label>Company name</label>
+                <input name="company-name"/>
+            </div>
+            <div class="col-md-12">
+                <button class="btn btn-primary">Update Profile</button>
+            </div>
+        </div>
+    <h3>Password</h3>
+    <div class="row form password">
+            <div class="col-md-12">
+                <label>Current password</label>
+                <input name="company-name"/>
+                <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-12">
+                <label>New password</label>
+                <input name="company-name"/>
+                <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-12">
+                <label>Re-enter new password</label>
+                <input name="company-name"/>
+                <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-12">
+                <button class="btn btn-primary">Change Password</button>
+            </div>
+    </div>
+    <h3>Account balance</h3>
+    <div class="row account-balance">
+        <div class="col-md-12 info">
+            <label>Available</label>
+            <div class="time">from 1 July, 2017 till 21 July, 2017</div>
+        </div>
+        <div class="col-md-12">
+            <div class="amount">
+                <div class="currency">EGP</div>
+                <div class="remaining">873</div>
+                <div class="total">From EGP 1,000</div>
+            </div>
+        </div>
+    </div>
+    <h3>Settings</h3>
+    <div class="row settings">
+        <div class="checkbox">
+        <input type="checkbox" value="">
+        <label>
+        <span>Notify me every</span>
+        <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Week
+              <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </button>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Week</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Month<</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Day<</a></li>
+            <li role="presentation" class="divider"></li>
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Year<</a></li>
+            </ul>
+        </div>
+        <span>with my upcoming favorites</span>
+        </label>
+        </div>
+        <div class="checkbox">
+        <input type="checkbox" value="">
+        <label>
+        <span>Receive reminder email in the</span>
+        <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown"> AM - 11 AM
+              <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </button>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">9 AM - 11 AM</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">10 AM - 12 AM<</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">9 AM - 11 AM<</a></li>
+              <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">9 AM - 11 AM<</a></li>
+            </ul>
+        </div>
+        </label>
+        </div>
+        <div class="checkbox">
+        <input type="checkbox">
+        <label>Receive special offers / event emails</label>
+        </div>
+    </div>
+  </div>
 <?php require_once('footer.php');?>
--- new-design/header.php
+++ new-design/header.php
@@ -3,39 +3,48 @@
   <title>Mumm</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet"> 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Muli|Titillium+Web" rel="stylesheet">  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
 <header>
   <div class="container">
-  <div class="row">
-    <div class="col-md-6 logo">
-      <img src="assets/img/logo.png" alt="logo"/>
-    </div>
-    <div class="col-md-2">
-      <button class="btn btn-primary">Schedule Order</button>
-    </div>
-    <div class="col-md-2">
-      <div class="account-info">
-        <div class="name">Mohamed Ahmed</div>
-        <div class="balance">
-          <label>Available Balance</label>
-          <span>EGP 873</span>
+    <div class="row">
+      <div class="col-md-6 logo">
+        <img src="assets/img/logo.png" alt="logo"/>
+      </div>
+      <div class="col-md-2">
+        <button class="btn btn-primary">Schedule Order</button>
+      </div>
+      <div class="col-md-2">
+        <div class="account-info">
+          <div class="name">Mohamed Ahmed</div>
+          <div class="balance">
+            <label>Available Balance</label>
+            <span>EGP 873</span>
+          </div>
+        </div>
+        <div class="expand-account">
+            <button type="button" class="btn" data-toggle="collapse" data-target="#my-account">
+              <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </button>
+          <div class="my-account-list collapse" id="my-account">
+          <ul>
+            <li><a href="#">My Orders</a></li>
+            <li><a href="#">My Favorites</a></li>
+            <li><a href="#">My Account</a></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
+        </div>
         </div>
       </div>
-      <div class="expand-account">
-          <i class="fa fa-caret-down" aria-hidden="true"></i>
+      <div class="col-md-1">
+        <div class="cart">
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </div>
       </div>
-    </div>
-    <div class="col-md-1">
-      <div class="cart">
-        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
       </div>
-    </div>
     </div>
   </div>
 </header>
-
