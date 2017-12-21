a196f86e9eacc64f1400807d0e402acbe9c8ccbe
Nancy <nancyhenry89@gmail.com>
2017-12-21 16:33:16 +0200

    Favorites page

---
+++ new-design/my-favorites.php
@@ -0,0 +1,108 @@
+<?php require_once('header.php'); ?>
+  <div class="container favorites">
+    <div class="container fav-header">
+      <div class="bread-crumb">
+        <a href="#">Home</a>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+        <a href="#">My Favorites</a>
+      </div>
+      <h2>My Favorites</h2>
+    </div>
+    <div class="container fav-content">
+      <div class="row fav-item">
+        <div class="col-md-4 fav-img-cont">
+          <img class="fav-img" src="" alt="Dish Image"/>
+        </div>
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-9">
+              <div class="dish-title">Paleo Grilled Chicken Bowl</div>
+              <div class="rating">
+                <div class="rating-stars">
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star-half" aria-hidden="true"></i>
+                </div>
+                <div class="rating-number">4.5</div>
+              </div>
+              <div class="sep">|</div>
+              <div class="dish-owner">
+                  by Manal’s Kitchen
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="currency">EGP</div>
+              <div class="price">95</div>
+            </div>
+          </div>
+          <div class="row dish-desc">
+            <div class="col-md-12">
+            This healthy bowl is served chilled and includes grilled and sliced chicken breast, massaged kale, roasted red bell, roasted sweet potato, shredded red cabbage, julienned daikon and tri-color carrots, roasted cauliflower, served with a lemon-tahini dressing.
+          </div>
+          </div>
+          <div class="row dish-tags">
+            <div class="col-md-12">
+              <div class="tag-item">
+                <span class="tag-icon"></span>
+                <span class="tag-text">Spicy</span>
+              </div>
+              <div class="tag-item">
+                <span class="tag-icon"><i class="fa fa-leaf" aria-hidden="true"></i></span>
+                <span class="tag-text">Healthy</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row fav-item">
+        <div class="col-md-4 fav-img-cont">
+          <img class="fav-img" src="" alt="Dish Image"/>
+        </div>
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-9">
+              <div class="dish-title">Paleo Grilled Chicken Bowl</div>
+              <div class="rating">
+                <div class="rating-stars">
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star-half" aria-hidden="true"></i>
+                </div>
+                <div class="rating-number">4.5</div>
+              </div>
+              <div class="sep">|</div>
+              <div class="dish-owner">
+                  by Manal’s Kitchen
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="currency">EGP</div>
+              <div class="price">95</div>
+            </div>
+          </div>
+          <div class="row dish-desc">
+            <div class="col-md-12">
+            This healthy bowl is served chilled and includes grilled and sliced chicken breast, massaged kale, roasted red bell, roasted sweet potato, shredded red cabbage, julienned daikon and tri-color carrots, roasted cauliflower, served with a lemon-tahini dressing.
+          </div>
+          </div>
+          <div class="row dish-tags">
+            <div class="col-md-12">
+              <div class="tag-item">
+                <span class="tag-icon"></span>
+                <span class="tag-text">Spicy</span>
+              </div>
+              <div class="tag-item">
+                <span class="tag-icon"><i class="fa fa-leaf" aria-hidden="true"></i></span>
+                <span class="tag-text">Healthy</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php require_once('footer.php');?>
