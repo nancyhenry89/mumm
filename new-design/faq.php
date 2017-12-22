d8e2e7002e3c6344084cf5d85368704d0ebe57ab
Nancy <nancyhenry89@gmail.com>
2017-12-23 00:31:21 +0200

    faq

---
+++ new-design/faq.php
@@ -0,0 +1,75 @@
+<?php require_once('header.php'); ?>
+    <div class="container faq">
+      <div class="row">
+        <div class="col-md-8">
+        <div class=" faq-header">
+          <div class="bread-crumb">
+            <a href="#">Home</a>
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+            <a href="#">FAQ</a>
+          </div>
+          <h2>Frequently Asked Question</h2>
+        </div>
+        <div class="">
+          <div class="row faq-content">
+            <div class="col-md-12">
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#Food">Food</a></li>
+              <li><a data-toggle="tab" href="#Cooks">Cooks</a></li>
+              <li><a data-toggle="tab" href="#Payment">Payment</a></li>
+              <li><a data-toggle="tab" href="#Account">Account</a></li>
+            </ul>
+          </div>
+            <div class="col-md-12">
+              <div class="tab-content">
+                <div id="Food" class="tab-pane fade in active">
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                </div>
+                <div id="Cooks" class="tab-pane fade">
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                </div>
+                <div id="Payment" class="tab-pane fade">
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                </div>
+                <div id="Account" class="tab-pane fade">
+                  <div class="faq-item">
+                    <div class="question">How long can I leave food out of the refrigerator?</div>
+                    <div class="answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-md-4">
+        <div class="contact">
+          <p>Your Question is not here?</p>
+          <button class="btn btn-primary">Contact us</button>
+        </div>
+      </div>
+  </div>
+</div>
+<?php require_once('footer.php');?>
