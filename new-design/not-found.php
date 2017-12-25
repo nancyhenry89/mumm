259366271fa61ade5e17abe63e78956f463edf5f
Nancy <nancyhenry89@gmail.com>
2017-12-25 20:20:04 +0200

    Not found

---
+++ new-design/not-found.php
@@ -0,0 +1,10 @@
+<?php require_once('personal-header.php'); ?>
+<div class="container-fluid not-found-bg">
+    <div class="container not-found text-center">
+      <img src="assets/img/not-found.png">
+      <h4>Nope, No food here!</h4>
+      <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+      <a href="home.php"><button class="btn btn-primary">Home</button></a>
+  </div>
+</div>
+<?php require_once('footer.php');?>
