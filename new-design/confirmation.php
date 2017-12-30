35d05b4fac3d658ec6acfda7f51c342840768ee8
Nancy <nancyhenry89@gmail.com>
2017-12-30 21:49:45 +0200

    Confirmation

---
+++ new-design/confirmation.php
@@ -0,0 +1,243 @@
+<?php require_once('header.php'); ?>
+    <div class="container confirmation">
+      <div class="container confirmation-header text-center">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="check-mark">
+              <i class="fa fa-check-circle" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <h2>Mohamed, Your order has been confirmed!</h2>
+          </div>
+          <div class="col-md-12">
+            Here is an overview of your order
+          </div>
+        </div>
+      </div>
+      <div class="container confirmation-content">
+        <div class="col-md-8">
+          <div class="row order-item">
+            <div class="col-md-2 order-date">
+              <div class="date">
+                <div class="number">3</div>
+                <div class="month">August</div>
+              </div>
+              <div class="day">Thursday</div>
+              <div class="time">3 PM - 4 PM</div>
+            </div>
+            <div class="col-md-10 order-detail">
+              <div class="row delivery-time">
+                <div>
+                  Your order will be delivered on <span>Wednesday, July 12 @ 3PM - 4PM</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 fav-img-cont">
+                  <img class="fav-img" src="assets/img/sample1.png" alt="Dish Image"/>
+                </div>
+                <div class="col-md-8">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="dish-title">Paleo Grilled Chicken Bowl</div>
+                      <div class="rating">
+                        <div class="rating-stars">
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star-half" aria-hidden="true"></i>
+                        </div>
+                        <div class="rating-number">4.5</div>
+                      </div>
+                      <div class="sep">|</div>
+                      <div class="dish-owner">
+                          by Manal’s Kitchen
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row dish-desc">
+                    <div class="col-md-11">
+                    This healthy bowl is served chilled and includes grilled and sliced chicken breast, massaged kale, roasted red bell, roasted sweet potato, shredded red cabbage, julienned daikon and tri-color carrots, roasted cauliflower, served with a lemon-tahini dressing.
+                  </div>
+                  <div class="col-md-1 dish-price">
+                    <div class="currency">EGP</div>
+                    <div class="price">95</div>
+                  </div>
+                  </div>
+                  <div class="row dish-tags">
+                    <div class="col-md-12">
+                      <div class="tag-item">
+                        <span class="tag-icon"></span>
+                        <span class="tag-text">Spicy</span>
+                      </div>
+                      <div class="tag-item">
+                        <span class="tag-icon"><i class="fa fa-leaf" aria-hidden="true"></i></span>
+                        <span class="tag-text">Healthy</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 fav-img-cont">
+                  <img class="fav-img" src="assets/img/sample2.png" alt="Dish Image"/>
+                </div>
+                <div class="col-md-8">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="dish-title">Spaghetti and Meatballs</div>
+                      <div class="rating">
+                        <div class="rating-stars">
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star-half" aria-hidden="true"></i>
+                        </div>
+                        <div class="rating-number">4.5</div>
+                      </div>
+                      <div class="sep">|</div>
+                      <div class="dish-owner">
+                          by Manal’s Kitchen
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row dish-desc">
+                    <div class="col-md-11">
+                    This healthy bowl is served chilled and includes grilled and sliced chicken breast, massaged kale, roasted red bell, roasted sweet potato, shredded red cabbage, julienned daikon and tri-color carrots, roasted cauliflower, served with a lemon-tahini dressing.
+                  </div>
+                  <div class="col-md-1 dish-price">
+                    <div class="currency">EGP</div>
+                    <div class="price">95</div>
+                  </div>
+                  </div>
+                  <div class="row dish-tags">
+                    <div class="col-md-12">
+                      <div class="tag-item">
+                        <span class="tag-icon"><i class="fa fa-heart" aria-hidden="true"></i></span>
+                        <span class="tag-text">Popular</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row order-item">
+            <div class="col-md-2 order-date">
+              <div class="date">
+                <div class="number">3</div>
+                <div class="month">August</div>
+              </div>
+              <div class="day">Thursday</div>
+              <div class="time">3 PM - 4 PM</div>
+            </div>
+            <div class="col-md-10 order-detail">
+              <div class="row delivery-time">
+                <div>
+                  Your order will be delivered on <span>Wednesday, July 12 @ 3PM - 4PM</span>
+                </div>
+              </div>
+      
+              <div class="row">
+                <div class="col-md-4 fav-img-cont">
+                  <img class="fav-img" src="assets/img/sample2.png" alt="Dish Image"/>
+                </div>
+                <div class="col-md-8">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="dish-title">Spaghetti and Meatballs</div>
+                      <div class="rating">
+                        <div class="rating-stars">
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                          <i class="fa fa-star-half" aria-hidden="true"></i>
+                        </div>
+                        <div class="rating-number">4.5</div>
+                      </div>
+                      <div class="sep">|</div>
+                      <div class="dish-owner">
+                          by Manal’s Kitchen
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row dish-desc">
+                    <div class="col-md-11">
+                    This healthy bowl is served chilled and includes grilled and sliced chicken breast, massaged kale, roasted red bell, roasted sweet potato, shredded red cabbage, julienned daikon and tri-color carrots, roasted cauliflower, served with a lemon-tahini dressing.
+                  </div>
+                  <div class="col-md-1 dish-price">
+                    <div class="currency">EGP</div>
+                    <div class="price">95</div>
+                  </div>
+                  </div>
+                  <div class="row dish-tags">
+                    <div class="col-md-12">
+                      <div class="tag-item">
+                        <span class="tag-icon"><i class="fa fa-heart" aria-hidden="true"></i></span>
+                        <span class="tag-text">Popular</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="order-summary">
+          <h3>Order Summary</h3>
+          <div class="confirmation-summary">
+            <div class="cart-date">Wednesday July 12, 2017</div>
+            <div class="cart-item row">
+              <div class="col-md-10 dish-name"><span>1x </span>Paleo Grilled Chicken Bowl</div>
+              <div class="col-md-2 dish-price">
+                <div class="currency">EGP</div>
+                <div class="number">95</div>
+              </div>
+            </div>
+            <div class="cart-item row">
+              <div class="col-md-10 dish-name"><span>1x </span>Paleo Grilled Chicken Bowl</div>
+              <div class="col-md-2 dish-price">
+                <div class="currency">EGP</div>
+                <div class="number">95</div>
+              </div>
+            </div>
+            <div class="cart-date">Wednesday July 12, 2017</div>
+            <div class="cart-item row">
+              <div class="col-md-10 dish-name"><span>1x </span>Paleo Grilled Chicken Bowl</div>
+              <div class="col-md-2 dish-price">
+                <div class="currency">EGP</div>
+                <div class="number">95</div>
+              </div>
+            </div>
+        </div>
+        <div class="confirmation-total row cart-item">
+            <div class="col-md-10 dish-name">Company Subsidy</div>
+            <div class="col-md-2 dish-price">
+              <div class="currency">EGP</div>
+              <div class="number">95</div>
+            </div>
+            <div class="col-md-10 dish-name">Subtotal</div>
+            <div class="col-md-2 dish-price">
+              <div class="currency">EGP</div>
+              <div class="number">95</div>
+            </div>
+        </div>
+        <div class="cart-item row confirmation-grand-total">
+          <div class="col-md-10 dish-name">Total</div>
+          <div class="col-md-2 dish-price">
+            <div class="currency">EGP</div>
+            <div class="number">95</div>
+          </div>
+          <div class="col-md-12 info">
+            Your balance after the order will be EGP 803
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+<?php require_once('footer.php');?>
