c133ccf167fd1e97a5c015f2c72674f109b85b3e
Nancy <nancyhenry89@gmail.com>
2018-01-17 13:09:15 +0200

    Personal confirmation page

---
+++ new-design/personal-confirmation.php
@@ -0,0 +1,98 @@
+<?php require_once('personal-header.php'); ?>
+<div class="container confirmation">
+   <div class="container  checkout-header confirmation-header text-center">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="checkout-steps">
+               <div class="step">
+                  <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                  <div>Cart</div>
+               </div>
+               <div class="step">
+                  <i class="fa fa-user" aria-hidden="true"></i>
+                  <div>Information</div>
+               </div>
+               <div class="step">
+                  <i class="fa fa-check-circle" aria-hidden="true"></i>
+                  <div>Confirmation</div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-12">
+            <div class="check-mark">
+               <i class="fa fa-check-circle" aria-hidden="true"></i>
+            </div>
+         </div>
+         <div class="col-md-12">
+            <h2>Mohamed, Your order has been confirmed!</h2>
+         </div>
+         <div class="col-md-12">
+         </div>
+      </div>
+   </div>
+   <div class="container personal-confirmation">
+     <div class="row text-center">
+       <div class="col-md-12">
+         <div class="p-order-owners">
+           <div class="p-order-owners-pics">
+             <img src="assets/img/ppl1.png"/>
+             <img src="assets/img/ppl1.png"/>
+           </div>
+           <div class="p-order-owners-names">
+             <p>Manal $ Zeinab are preparing your meal</p>
+         </div>
+       </div>
+     </div>
+   </div>
+   <div class="row text-center">
+     <div class="col-md-12">
+       <div class="gift">
+         <p>Gift your friends <b>EGP 20</b> and get <b>EGP 20</b> !</p>
+         <p>When they place their first order</p>
+       </div>
+     </div>
+     <div class="col-md-12">
+       <div class="gift-info">
+         <p>Ask your friends to use the invitation code below and get EGP 20 discount on your next order from Mumm</p>
+         <p>They'll get EGP 20 discount as well on their first order.</p>
+       </div>
+     </div>
+     <div class="col-md-12">
+       <div class="invitation-code">
+         <p>Your invitation code is</p>
+         <div class="code">3D907</div>
+       </div>
+     </div>
+     <div class="col-md-12">
+        <div class="row cook-share">
+           <div class="col-md-12 share-label">SHARE ON</div>
+           <div class="col-md-3">
+              <div class="share-item whatsapp">
+                 <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                 <span>Whatsapp</span>
+              </div>
+           </div>
+           <div class="col-md-3">
+              <div class="share-item twitter">
+                 <i class="fa fa-twitter" aria-hidden="true"></i>
+                 <span>Twitter</span>
+              </div>
+           </div>
+           <div class="col-md-3">
+              <div class="share-item facebook">
+                 <i class="fa fa-facebook" aria-hidden="true"></i>
+                 <span>Facebook</span>
+              </div>
+           </div>
+           <div class="col-md-3">
+              <div class="share-item linkedin">
+                 <i class="fa fa-linkedin" aria-hidden="true"></i>
+                 <span>Linkedin</span>
+              </div>
+           </div>
+        </div>
+     </div>
+   </div>
+</div>
+</div>
+<?php require_once('footer.php');?>
--- new-design/personal-header.php
+++ new-design/personal-header.php
@@ -27,49 +27,151 @@
                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>
                 <span class="cart-count">2</span>
               </button>
-            <div class="cart-details collapse" id="cart">
+            <div class="cart-details order-summary collapse" id="cart">
               <div class="cart-title">Your Cart</div>
-              <div class="cart-date">Wednesday July 12, 2017</div>
-              <div class="cart-item row">
-                <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
-                <div class="col-md-3 dish-quantity">
-                  <a href="#" class="minus">-</a>
-                  <label class="quantity-number"/>1</label>
-                  <a href="#" class="plus">+</a>
-                </div>
-                <div class="col-md-2 dish-price">
-                  <div class="currency">EGP</div>
-                  <div class="number">95</div>
-                </div>
-                <div class="col-md-1 remove-item"><a href="#">x</a></div>
+              <div class="cart-info">
+                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                <div class="cart-info-title">You have 2 orders in your cart!</div>
+                <div class="cart-info-detail">Each order will be delivered separatly, with separate delivery fees!</div>
               </div>
-              <div class="cart-item row">
-                <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
-                <div class="col-md-3 dish-quantity">
-                  <a href="" class="minus">-</a>
-                  <label class="quantity-number"/>1</label>
-                  <a href="" class="plus">+</a>
-                </div>
-                <div class="col-md-2 dish-price">
-                  <div class="currency">EGP</div>
-                  <div class="number">95</div>
-                </div>
-                <div class="col-md-1 remove-item"><a href="">x</a></div>
-              </div>
-              <div class="cart-date">Wednesday July 12, 2017</div>
-              <div class="cart-item row">
-                <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
-                <div class="col-md-3 dish-quantity">
-                  <a href="" class="minus">-</a>
-                  <label class="quantity-number"/>1</label>
-                  <a href="" class="plus">+</a>
-                </div>
-                <div class="col-md-2 dish-price">
-                  <div class="currency">EGP</div>
-                  <div class="number">95</div>
-                </div>
-                <div class="col-md-1 remove-item"><a>x</a></div>
-              </div>
+                 <div class="order-item">
+                    <div class="cook">
+                       <img src="assets/img/ppl1.png"/>
+                       <div class="cook-name">Manal’s Kitchen Order</div>
+                       <a class="link">Remove order</a>
+                    </div>
+                    <div class="cook-item cart-item">
+                       <div class="col-md-3 dish-quantity">
+                          <a href="#" class="minus">-</a>
+                          <label class="quantity-number"/>1</label>
+                          <a href="#" class="plus">+</a>
+                       </div>
+                       <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
+                       <div class="col-md-2 dish-price">
+                          <div class="currency">EGP</div>
+                          <div class="number">95</div>
+                       </div>
+                       <div class="col-md-1 remove-item"><a href="#">x</a></div>
+                    </div>
+                    <div class="cook-item cart-item">
+                       <div class="col-md-3 dish-quantity">
+                          <a href="#" class="minus">-</a>
+                          <label class="quantity-number"/>1</label>
+                          <a href="#" class="plus">+</a>
+                       </div>
+                       <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
+                       <div class="col-md-2 dish-price">
+                          <div class="currency">EGP</div>
+                          <div class="number">95</div>
+                       </div>
+                       <div class="col-md-1 remove-item"><a href="#">x</a></div>
+                    </div>
+                    <div class="price-cook">
+                       <div class="item">
+                          <div class="description">First Kitchen fees</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">70</div>
+                          </div>
+                       </div>
+                       <div class="item">
+                          <div class="description">First Kitchen fees</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">70</div>
+                          </div>
+                       </div>
+                       <div class="item">
+                          <div class="description">14% VAT</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">70</div>
+                          </div>
+                       </div>
+                       <div class="payment-total">
+                          <div class="description">Total</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">160</div>
+                          </div>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="order-item">
+                    <div class="cook">
+                       <img src="assets/img/ppl1.png"/>
+                       <div class="cook-name">Manal’s Kitchen Order</div>
+                       <a class="link">Remove order</a>
+                    </div>
+                    <div class="cook-item cart-item">
+                       <div class="col-md-3 dish-quantity">
+                          <a href="#" class="minus">-</a>
+                          <label class="quantity-number"/>1</label>
+                          <a href="#" class="plus">+</a>
+                       </div>
+                       <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
+                       <div class="col-md-2 dish-price">
+                          <div class="currency">EGP</div>
+                          <div class="number">95</div>
+                       </div>
+                       <div class="col-md-1 remove-item"><a href="#">x</a></div>
+                    </div>
+                    <div class="cook-item cart-item">
+                       <div class="col-md-3 dish-quantity">
+                          <a href="#" class="minus">-</a>
+                          <label class="quantity-number"/>1</label>
+                          <a href="#" class="plus">+</a>
+                       </div>
+                       <div class="col-md-6 dish-name">Paleo Grilled Chicken Bowl</div>
+                       <div class="col-md-2 dish-price">
+                          <div class="currency">EGP</div>
+                          <div class="number">95</div>
+                       </div>
+                       <div class="col-md-1 remove-item"><a href="#">x</a></div>
+                    </div>
+                    <div class="price-cook">
+                       <div class="item">
+                          <div class="description">Subtotal</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">70</div>
+                          </div>
+                       </div>
+                       <div class="item">
+                          <div class="description">First Kitchen delivery fees</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">70</div>
+                          </div>
+                       </div>
+                       <div class="item">
+                          <div class="description">14% VAT</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">70</div>
+                          </div>
+                       </div>
+                       <div class="payment-total">
+                          <div class="description">Total</div>
+                          <div class="price">
+                             <div class="currency">EGP</div>
+                             <div class="number">160</div>
+                          </div>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="grand-total">
+                    <div class="payment-total">
+                       <div class="description">Total</div>
+                       <div class="price">
+                          <div class="currency">EGP</div>
+                          <div class="number">160</div>
+                       </div>
+                    </div>
+                    <div class="cart-checkout">
+                      <button class="btn btn-primary">Continue to checkout</button>
+                    </div>
+                 </div>
           </div>
           </div>
         </div>
